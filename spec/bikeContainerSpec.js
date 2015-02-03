7a6fc1de594f9fc697f4cbe3566cc0d27232638f
Danielle Demkiw <ddemkiw@gmail.com>
2015-02-03 17:23:08 +0000

    adding bike container functions and tests

--- spec/bikeContainerSpec.js
+++ spec/bikeContainerSpec.js
@@ -1,26 +1,74 @@
 var BikeContainer = require('../src/bikeContainer.js');
 var Bike = require('../src/bike.js');
+var _ = require('underscore')._;
 
 describe ('BikeContainer', function(){
+
+  var dockBrokenBike, bike, bike2, bike3, container  
 
   beforeEach(function(){
     container = new BikeContainer();
     bike = new Bike();
+    bike2 = new Bike();
   });
+
+   dockBrokenBike = function () {
+      bike3 = new Bike();
+      bike3.break();
+      container.dock(bike3);
+  };
+
+  dockTenBikes = function(){
+    var i = 0;
+    while(i < 10){
+      bike = new Bike();
+      container.dock(bike);
+      i++;
+    }
+  };
+
 
   it('can accept bikes', function(){
     container.dock(bike);
-    expect(container.bikes.length).toEqual(1);
+    expect(container.bikeCount()).toEqual(1);
   });
 
   it('can release bikes', function(){
     container.dock(bike);
     container.release();
-    expect(container.bikes.length).toEqual(0)
+    expect(container.bikeCount()).toEqual(0)
+  });
+
+  it('knows how many bikes it contains', function(){
+    dockBrokenBike();
+    container.dock(bike);
+    container.dock(bike2);
+    expect(container.bikeCount()).toEqual(3);
+  });
+
+  it('knows when it is full', function(){
+    dockTenBikes();
+    expect(container._isFull()).toBe(true);
+  });
+
+  it('should not accept more bikes when it is full', function(){
+    dockTenBikes();
+    container.dock(bike2);
+    expect(container.bikeCount()).toEqual(10);
+
+  });
+
+  it('should provide a list of broken bikes', function(){
+    dockBrokenBike();
+    container.dock(bike);
+    expect(container.brokenBikes().length).toEqual(1);
   });
 
   it('should provide a list of available bikes', function(){
-
+    dockBrokenBike();
+    container.dock(bike);
+    container.dock(bike2);
+    expect(container.availableBikes().length).toEqual(2);
   });
 
 });
--- src/bikeContainer.js
+++ src/bikeContainer.js
@@ -1,16 +1,39 @@
 var Bike = require('./bike');
+var _ = require('underscore')._;
 
 function BikeContainer(){
   this.bikes = [];
+  this.capacity = 10;
 };
 
 BikeContainer.prototype.dock = function(bike) {
-  this.bikes.push(bike);
+  if(this._isFull()){
+    return this.bikes
+  }else{
+  this.bikes.push(bike)
+  };
 };
 
 BikeContainer.prototype.release = function() {
   this.bikes.shift();
 };
 
+BikeContainer.prototype.bikeCount = function(){
+  return this.bikes.length;
+};
+
+BikeContainer.prototype.brokenBikes = function() {
+ var broken = _.filter(this.bikes, function(bike){ return bike.isBroken()});
+ return broken;
+};
+
+BikeContainer.prototype.availableBikes = function() {
+  var readyBikes = _.filter(this.bikes, function(bike){ return !bike.isBroken(); });
+  return readyBikes;
+};
+
+BikeContainer.prototype._isFull = function() {
+  return this.capacity === this.bikes.length; 
+};
 
 module.exports = BikeContainer;
